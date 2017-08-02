3f63b6f93427c4d6f46092187d3de1a2674705cd
Jan Stancek <jstancek@redhat.com>
2017-08-02 11:33:21 +0200

    try various combinations of data offsets and sizes
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- sha1_test.c
+++ sha1_test.c
@@ -21,7 +21,6 @@ static void *calc_hash(const char *hashname, u8 *d, int len1, int len2)
 	struct sha1_state *sctx;
 	static int digest_size;
 
-	printk("starting test for %s\n", hashname);
 	tfm = crypto_alloc_shash(hashname, 0, 0);
 	if (IS_ERR(tfm)) {
 		printk("failed to alloc %s\n", hashname);
@@ -47,7 +46,6 @@ static void *calc_hash(const char *hashname, u8 *d, int len1, int len2)
 	d += len1;
 
 	sctx = shash_desc_ctx(desc);
-	printk("count: %llu\n", sctx->count);
 
 	ret = crypto_shash_update(desc, d, len2);
 	if (ret < 0)
@@ -62,12 +60,38 @@ error:
 
 int (*set_memory_np)(unsigned long addr, int numpages);
 
+void test_hash(char *hashname, u8 *data, int maxlen)
+{
+	int so, chunk1, chunk2, chunk2_min, chunk2_max;
+	long hashes = 0;
+	void *ptr;
+
+	printk("sha_test testing hash: %s, maxlen: %d\n", hashname, maxlen);
+	for (so = PAGE_SIZE - 128; so < PAGE_SIZE; so++) {
+		// printk("  start_offset: %d\n", so);
+
+		for (chunk1 = PAGE_SIZE - 128; chunk1 < PAGE_SIZE; chunk1++) {
+			chunk2_max = maxlen - so - chunk1;
+			chunk2_min = chunk2_max > 128 ? chunk2_max - 128 : 1;
+			for (chunk2 = chunk2_min; chunk2 <= chunk2_max; chunk2++) {
+				ptr = calc_hash(hashname, data + so, chunk1, chunk2);
+				if (IS_ERR(ptr))
+					return;
+				hashes++;
+			}
+		}
+	}
+	printk("sha_test hash: %s calculated %ld hashes\n", hashname, hashes);
+}
+
 static int __init sha1test_init(void)
 {
-	int len, datalen, start_offset;
+	int maxlen;
 	u8 *page_after_data;
 	u8 *data;
-	
+	char *hashes[] = { "sha1-generic", "sha1-ni", "sha1-avx", "sha1-avx2", "sha1-ssse3", NULL };
+	char **hash = hashes;
+
 	printk("sha_test module loaded\n");
 	set_memory_np = (void *) kallsyms_lookup_name("set_memory_np");
 	if (!set_memory_np) {
@@ -80,34 +104,31 @@ static int __init sha1test_init(void)
 		return -EINVAL;
 	}
 
-	start_offset = PAGE_SIZE - 148; 
-	len = 148 + PAGE_SIZE;
-	datalen = PAGE_ALIGN(start_offset + len) + PAGE_SIZE;
-	data = kmalloc(datalen, GFP_KERNEL);
+	maxlen = PAGE_SIZE * 2;
+	data = kmalloc(maxlen + PAGE_SIZE, GFP_KERNEL);
+	page_after_data = data + maxlen;
+	printk("data is at 0x%p, page_after_data: 0x%p\n",
+		data, page_after_data);
 
-	printk("data is at 0x%p, datalen: %d, start_offset: %d, last_byte: 0x%p\n",
-		data, datalen, start_offset, data + start_offset + len - 1);
-	page_after_data = PTR_ALIGN(data + start_offset + len, PAGE_SIZE);
-	printk("page_after_data is at 0x%p\n", page_after_data);
-
-	/* 
- 	 * We have 3 pages, hash should use only first 2, we marked
- 	 * 3rd one as not present
- 	 *  +----------------+------------------+-----------------+
- 	 *         PAGE1             PAGE2             PAGE3
- 	 *  +----------------+------------------+-----------------+
- 	 *  ^ data
+	/*
+ 	 * We have X pages, which hash should use, we marked page after
+ 	 * last one as not present.
+ 	 *  +----------------+...------------------+-----------------+
+ 	 *         PAGE1      ...       PAGEX             PAGEX+1
+ 	 *  +----------------+...------------------+-----------------+
+ 	 *  ^ data                                    (not present)
  	 *                 ^ start_offset
- 	 *                                     ^ last_byte shash should use
- 	 *                                       
+ 	 *                                   ^ last_byte shash should use
+ 	 *
  	 */
 	set_memory_np((unsigned long)page_after_data, 1);
 
-	calc_hash("sha1-generic", data + start_offset, 148, PAGE_SIZE);
-	calc_hash("sha1-ni", data + start_offset, 148, PAGE_SIZE);
-	calc_hash("sha1-avx", data + start_offset, 148, PAGE_SIZE);
-	calc_hash("sha1-avx2", data + start_offset, 148, PAGE_SIZE);
-	calc_hash("sha1-ssse3", data + start_offset, 148, PAGE_SIZE);
+	while (*hash) {
+		test_hash(*hash, data, maxlen);
+		hash++;
+	}
+
+	printk("sha_test done\n");
 
 	/* yes, it leaks memory */
 	return 0;
